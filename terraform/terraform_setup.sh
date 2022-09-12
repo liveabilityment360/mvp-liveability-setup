5bf9efb7200d48b1f4c3570a9009a8864f3480ef
liveabilityment360 <109857290+liveabilityment360@users.noreply.github.com>
2022-09-13 02:23:24 +1000

    Update terraform_setup.sh

--- terraform/terraform_setup.sh
+++ terraform/terraform_setup.sh
@@ -5,17 +5,16 @@ export LOCATION="australia-southeast1"
 export GOOGLE_APPLICATION_CREDENTIALS=../key.json
 export MYSQL_INSTANCE="aus-liveability-demo-mysql"
 
-
-#12.Create the SQL instance using terraform
+#Create the SQL instance using terraform
 terraform init
 terraform plan    
 terraform apply -auto-approve
 cd 
 
-#16.Datastream user creation replica process and allowing privilleges for the 'datastream' user.
+#Datastream user creation replica process and allowing privilleges for the 'datastream' user.
 SERVICE_ACCOUNT=$(gcloud sql instances describe ${MYSQL_INSTANCE} | grep serviceAccountEmailAddress | awk '{print $2;}')
 gsutil iam ch serviceAccount:$SERVICE_ACCOUNT:objectViewer gs://${PROJECT_ID}
 gcloud sql import sql ${MYSQL_INSTANCE} gs://${PROJECT_ID}/ddl/create-ds-user-privileges.sql --quiet
 
-#17.Create table in mysql
+#Create table in mysql
 gcloud sql import sql ${MYSQL_INSTANCE} gs://${PROJECT_ID}/ddl/ddl_user_activity.sql --quiet
