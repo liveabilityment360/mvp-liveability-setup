9fc73aced73009fee4d48e421f06d89fc07461ca
liveabilityment360 <109857290+liveabilityment360@users.noreply.github.com>
2022-09-16 01:53:55 +1000

    Update create_tables.sh

--- ddl/create_tables.sh
+++ ddl/create_tables.sh
@@ -3,10 +3,10 @@ export PROJECT_ID="mvp-liveability-setup"
 gcloud config set project ${PROJECT_ID}
 export LOCATION="australia-southeast1"
 
-#export GOOGLE_APPLICATION_CREDENTIALS=../key.json
+export GOOGLE_APPLICATION_CREDENTIALS=../key.json
 #export GOOGLE_APPLICATION_CREDENTIALS= gs://${PROJECT_ID}/json_key/key.json
-gsutil cp gs://${PROJECT_ID}/json_key/key.json .
-export GOOGLE_APPLICATION_CREDENTIALS=key.json
+#gsutil cp gs://${PROJECT_ID}/json_key/key.json .
+#export GOOGLE_APPLICATION_CREDENTIALS=key.json
 
 export MYSQL_INSTANCE="aus-liveability-demo-mysql"
 
