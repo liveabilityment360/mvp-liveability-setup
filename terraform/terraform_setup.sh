8d25af62b56aea81b1e9251d96fb690b3b2a7934
liveabilityment360 <109857290+liveabilityment360@users.noreply.github.com>
2022-09-15 20:42:51 +1000

    Update terraform_setup.sh

--- terraform/terraform_setup.sh
+++ terraform/terraform_setup.sh
@@ -1,5 +1,5 @@
 #1.create a project and export it into a variable(project creation- manual)
-export PROJECT_ID="mvp-liveability-setup-alpha"
+export PROJECT_ID="mvp-liveability-setup"
 gcloud config set project ${PROJECT_ID}
 export LOCATION="australia-southeast1"
 
