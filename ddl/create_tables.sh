0d28610aa64ebb5055e12354ffe5ad4774e780c3
liveabilityment360 <109857290+liveabilityment360@users.noreply.github.com>
2022-09-15 16:57:31 +1000

    Add files via upload

--- ddl/create_tables.sh
+++ ddl/create_tables.sh
@@ -1,8 +1,14 @@
-export PROJECT_ID="mvp-liveability-setup-alpha"
+
+export PROJECT_ID="proj-liveability"
 gcloud config set project ${PROJECT_ID}
 export LOCATION="australia-southeast1"
-export GOOGLE_APPLICATION_CREDENTIALS=../key.json
-export MYSQL_INSTANCE="mvp-liveability"
+
+#export GOOGLE_APPLICATION_CREDENTIALS=../key.json
+#export GOOGLE_APPLICATION_CREDENTIALS= gs://${PROJECT_ID}/json_key/key.json
+gsutil cp gs://${PROJECT_ID}/json_key/key.json .
+export GOOGLE_APPLICATION_CREDENTIALS=key.json
+
+export MYSQL_INSTANCE="aus-liveability-demo-mysql"
 
 #Datastream user creation replica process and allowing privilleges for the 'datastream' user.
 SERVICE_ACCOUNT=$(gcloud sql instances describe ${MYSQL_INSTANCE} | grep serviceAccountEmailAddress | awk '{print $2;}')
@@ -11,7 +17,6 @@ gcloud sql import sql ${MYSQL_INSTANCE} gs://${PROJECT_ID}/ddl/create-ds-user-pr
 
 #Create table in mysql
 gcloud sql import sql ${MYSQL_INSTANCE} gs://${PROJECT_ID}/ddl/ddl_user_activity.sql --quiet
-
 
 #create childcare center table
 bq mk -t --description "childcare centers"  \liveability.childcarecenters  Name:STRING,Categories:STRING,Address:STRING,City:STRING,State:STRING,Postcode:STRING,Phone:STRING,Fax:STRING,Latitude:STRING,Longitude:STRING,Employees:STRING,Established:STRING,ABN_Status:STRING,ABN:STRING,ABN_Name:STRING,Accuracy:STRING,ABN_2:STRING,ACN:STRING,Created_Date:STRING
@@ -33,4 +38,3 @@ bq mk -t --description "shoppingcentres"  \liveability.shoppingcentres  Name:STR
 
 #create sportsclubs table
 bq mk -t --description "sportsclubs"  \liveability.sportsclubs  Name:STRING,Categories:STRING,Address:STRING,City:STRING,State:STRING,Postcode:STRING,Phone:STRING,Website:STRING,Email:STRING,Fax:STRING,Latitude:STRING,Longitude:STRING,Employees:STRING,Established:STRING,Licence_No:STRING,ABN_Status:STRING,ABN:STRING,ABN_Name:STRING,Accuracy:STRING,ABN_2:STRING,ACN:STRING,Created_Date:STRING
-
